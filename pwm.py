b7cdb3e77e4e89eb95d507e88b187c36b1d357e0
anushakolluru <57355282+anushakolluru@users.noreply.github.com>
2020-02-26 15:04:58 +0530

    Update pwm.py

--- pwm.py
+++ pwm.py
@@ -7,7 +7,7 @@ GPIO.setup(16, GPIO.OUT)
 GPIO.setup(26, GPIO.OUT)
 GPIO.setup(36, GPIO.OUT)
 
-GPIO.setup(15, GPIO.OUT)
+
 
 
 GPIO.output(12, 1)
@@ -15,8 +15,4 @@ GPIO.output(16, 1)
 GPIO.output(26, 1)
 GPIO.output(36, 0)
 
-GPIO.output(16, 0)
-
-#no=(0<<3)+(1<<2)+(1<<1)+(1<<0);
-#GPIO.output(12,no)
 
